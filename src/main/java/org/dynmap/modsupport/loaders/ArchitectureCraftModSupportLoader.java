d5ffa5970cc9ac10233af0b58786dd13cc0786bf
David Lindström <info@davidlindstrom.se>
2023-04-21 01:33:02 +0200

    Update the current ModSupportLoader implementations to the new interface

--- src/main/java/org/dynmap/modsupport/loaders/ArchitectureCraftModSupportLoader.java
+++ src/main/java/org/dynmap/modsupport/loaders/ArchitectureCraftModSupportLoader.java
@@ -4,9 +4,11 @@ import org.dynmap.DynmapCore;
 import org.dynmap.modsupport.GWM_Util;
 import org.dynmap.renderer.CustomModSupportLoader;
 
+import java.util.HashMap;
+
 public class ArchitectureCraftModSupportLoader extends CustomModSupportLoader {
     @Override
-    public void initializeModSupport(DynmapCore core) {
+    public void initializeModSupport(DynmapCore core, HashMap<String, Integer> filetoidx) {
         GWM_Util.initialize(core);
     }
 }
--- src/main/java/org/dynmap/modsupport/loaders/GregTechModSupportLoader.java
+++ src/main/java/org/dynmap/modsupport/loaders/GregTechModSupportLoader.java
@@ -2,11 +2,34 @@ package org.dynmap.modsupport.loaders;
 
 import org.dynmap.DynmapCore;
 import org.dynmap.modsupport.GWM_Util;
+import org.dynmap.modsupport.gregtech.GregTechSupport;
 import org.dynmap.renderer.CustomModSupportLoader;
 
+import java.util.HashMap;
+
 public class GregTechModSupportLoader extends CustomModSupportLoader {
+    HashMap<String, Integer> filetoidx;
     @Override
-    public void initializeModSupport(DynmapCore core) {
+    public void initializeModSupport(DynmapCore core, HashMap<String, Integer> filetoidx) {
         GWM_Util.initialize(core);
+        this.filetoidx = filetoidx;
+    }
+
+    @Override
+    public void processData(String line, HashMap<String, String> data) {
+        String type = data.get("type");
+
+        if(type == null)
+            return;
+
+        if(type.equals("iconset")){
+            GregTechSupport.INSTANCE.processIconSet(data, filetoidx);
+        }
+        else if(type.equals("mte")){
+            GregTechSupport.INSTANCE.processMte(data, filetoidx);
+        }
+        else if(type.equals("hatchbase")){
+            GregTechSupport.INSTANCE.processHatchBase(data, filetoidx);
+        }
     }
 }
