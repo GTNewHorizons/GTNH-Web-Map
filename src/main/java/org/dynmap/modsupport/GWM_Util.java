95d988e1e238003a4f630b0998d4199ab6ec9ed5
David Lindström <info@davidlindstrom.se>
2023-06-28 20:05:41 +0200

    Add unlocalized name to block id map, and add overload to blockNameToId to allow using that map as backup

--- src/main/java/org/dynmap/modsupport/GWM_Util.java
+++ src/main/java/org/dynmap/modsupport/GWM_Util.java
@@ -1,28 +1,48 @@
 package org.dynmap.modsupport;
 
+import cpw.mods.fml.common.registry.FMLControlledNamespacedRegistry;
+import cpw.mods.fml.common.registry.GameData;
+import net.minecraft.block.Block;
 import net.minecraft.client.stream.IngestServerTester;
 import org.dynmap.DynmapCore;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class GWM_Util {
     static boolean initialized;
     static Map<String, Integer> blockIdMap;
+    static Map<String, Integer> unlocalizedNameToIdMap;
     public static void initialize(DynmapCore core)
     {
         if(initialized)
             return;
 
         blockIdMap = core.getBlockIDMap();
+        unlocalizedNameToIdMap = new HashMap<>();
+
+        FMLControlledNamespacedRegistry<Block> blockRegistry = GameData.getBlockRegistry();
+        for (Map.Entry<String, Integer> entry : blockIdMap.entrySet()) {
+            Block block = blockRegistry.getObjectById(entry.getValue());
+            unlocalizedNameToIdMap.put(block.getUnlocalizedName(), entry.getValue());
+        }
 
         initialized = true;
     }
-
-    public static int blockNameToId(String stringId)
+    public static int blockNameToId(String stringId){
+        return blockNameToId(stringId, false);
+    }
+    public static int blockNameToId(String stringId, boolean alsoCheckUnlocalizedNames)
     {
         Integer intId = blockIdMap.get(stringId);
         if(intId != null)
             return intId.intValue();
+
+        if(alsoCheckUnlocalizedNames) {
+            intId = unlocalizedNameToIdMap.get(stringId);
+            if (intId != null)
+                return intId.intValue();
+        }
 
         return 0;
     }
