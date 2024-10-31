743d2e085f417d3b1ffa7f4d20fe9f5ab4516030
David Lindström <info@davidlindstrom.se>
2024-11-01 00:58:50 +0100

    Added new helper for getting boxes in CustomRenderer implementations

--- src/main/java/org/dynmap/renderer/CustomRenderer.java
+++ src/main/java/org/dynmap/renderer/CustomRenderer.java
@@ -33,7 +33,20 @@ public abstract class CustomRenderer {
     protected static RenderPatch[] getBoxSingleTextureInt(RenderPatchFactory rpf, int xmin, int xmax, int ymin, int ymax, int zmin, int zmax, int tex, boolean rotateTopAndBottom) {
         return getBoxSingleTexture(rpf, xmin/16.0, xmax/16.0, ymin/16.0,ymax/16.0,zmin/16.0,zmax/16.0,tex, rotateTopAndBottom);
     }
+    protected static RenderPatch[] getBoxMultiTexture(RenderPatchFactory rpf, double xmin, double xmax, double ymin, double ymax, double zmin, double zmax, int startTex) {
+        int[] patchTextureIds = new int[]{startTex, startTex + 1, startTex + 2, startTex + 3, startTex + 4, startTex + 5};
 
+        return getBoxMultiTexture(rpf, xmin, xmax, ymin, ymax, zmin, zmax, patchTextureIds);
+    }
+
+    protected static RenderPatch[] getBoxMultiTexture(RenderPatchFactory rpf, double xmin, double xmax, double ymin, double ymax, double zmin, double zmax, int[] patchTextureIds) {
+        ArrayList<RenderPatch> list = new ArrayList<RenderPatch>();
+        CustomRenderer.addBox(rpf, list, xmin, xmax, ymin, ymax, zmin, zmax, patchTextureIds);
+        return list.toArray(new RenderPatch[patchTextureIds.length]);
+    }
+    protected static RenderPatch[] getBoxMultiTextureInt(RenderPatchFactory rpf, int xmin, int xmax, int ymin, int ymax, int zmin, int zmax, int startTex) {
+        return getBoxMultiTexture(rpf, xmin/16.0, xmax/16.0, ymin/16.0,ymax/16.0,zmin/16.0,zmax/16.0, startTex);
+    }
     protected static RenderPatch[] getBoxFull(RenderPatchFactory rpf, int[] patchIndices) {
         ArrayList<RenderPatch> list = new ArrayList<RenderPatch>();
         CustomRenderer.addBox(rpf, list, 0, 1, 0, 1, 0, 1, patchIndices);
